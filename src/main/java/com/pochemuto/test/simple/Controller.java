def222759087c6f29adea16cb0c2e59bfd87f8a1
Alexander Kramarev <pochemuto@yandex-team.ru>
2021-04-30 00:09:26 +0300

    remove cron, add deployment

--- src/main/java/com/pochemuto/test/simple/Controller.java
+++ src/main/java/com/pochemuto/test/simple/Controller.java
@@ -1,16 +1,22 @@
 package com.pochemuto.test.simple;
 
+import java.time.Instant;
 import java.util.Optional;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class Controller {
+    private final TestRepository repository;
+
     @GetMapping({"add/{name}", "add"})
     public String add(@PathVariable(required = false) Optional<String> name) {
-        String person = name.orElse("nobody");
-        return person + " was added";
+        var person = new Person(name.orElse("nobody"), Instant.now());
+        repository.save(person);
+        return person.id() + " was added at " + person.updated();
     }
 }
