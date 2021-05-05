984b3fadd74996b0a59cd395305c3949cdba9ee2
Alexander Kramarev <pochemuto@yandex-team.ru>
2021-05-05 17:50:40 +0300

    simplify configuration

--- src/main/java/com/pochemuto/test/simple/Controller.java
+++ src/main/java/com/pochemuto/test/simple/Controller.java
@@ -4,12 +4,14 @@ import java.time.Instant;
 import java.util.Optional;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class Controller {
     private final TestRepository repository;
 
@@ -20,6 +22,7 @@ public class Controller {
 
     @GetMapping({"add/{name}", "add"})
     public String add(@PathVariable(required = false) Optional<String> name) {
+        log.info("Got name: " + name);
         var person = new Person(name.orElse("nobody"), Instant.now());
         repository.save(person);
         return person.id() + " was added at " + person.updated();
