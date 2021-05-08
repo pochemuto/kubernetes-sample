6e91951cce47f4b841384f3ca5b4093984c043f5
Alexander Kramarev <pochemuto@yandex-team.ru>
2021-05-08 14:33:17 +0300

    add configMap

--- src/main/java/com/pochemuto/test/simple/Controller.java
+++ src/main/java/com/pochemuto/test/simple/Controller.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
